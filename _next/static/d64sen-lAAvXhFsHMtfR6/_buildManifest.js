abd8766b11df076bddce9e554031c7718c159ff1
WuChenDi <wuchendi96@gmail.com>
2022-10-11 10:18:31 +0000

    Deploying to gh-pages from @ WuChenDi/nextjs-tailwindcss-examples@501db262a93613ebda66a48da1e6ceaa137d98c6 🚀

---
+++ _next/static/d64sen-lAAvXhFsHMtfR6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dda030fdbb24f4fb.js"],"/_error":["static/chunks/pages/_error-fa2da97f07e0d864.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
